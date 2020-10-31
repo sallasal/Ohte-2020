0f320dd65210772647008ebea2edb2d0accfd9c6
Salla S <47078225+sallasal@users.noreply.github.com>
2020-10-31 18:03:45 +0200

    Week 2 exercise 2: add tests to Unicafe

--- laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
+++ laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
@@ -17,4 +17,42 @@ public class MaksukorttiTest {
     public void luotuKorttiOlemassa() {
         assertTrue(kortti!=null);      
     }
+    
+    @Test
+    public void alkusaldoOikein() {
+        assertEquals("saldo: 0.10", kortti.toString());
+    }
+    
+    @Test
+    public void latausToimiiOikein() {
+        kortti.lataaRahaa(100);
+        //Kortilla nyt saldoa 1 euro 10 senttiä
+        assertEquals("saldo: 1.10", kortti.toString());
+    }
+    
+    @Test
+    public void ottoToimiiKunRahaaOn() {
+        kortti.lataaRahaa(1000);
+        //Kortilla on nyt rahaa 10 euroa ja 10 senttiä
+        kortti.otaRahaa(510);
+        assertEquals("saldo: 5.0", kortti.toString());
+    }
+    
+    @Test
+    public void ottoToimiiKunRahaaEiOle() {
+        kortti.otaRahaa(510);
+        assertEquals("saldo: 0.10", kortti.toString());
+    }
+    
+    @Test
+    public void palautusOikeinKunRahaaOn() {
+        boolean palautus = kortti.otaRahaa(5);
+        assertEquals(true,palautus);
+    }
+    
+    @Test
+    public void palautusOikeinKunRahaaEiOle() {
+        boolean palautus = kortti.otaRahaa(500);
+        assertEquals(false,palautus);
+    }
 }
