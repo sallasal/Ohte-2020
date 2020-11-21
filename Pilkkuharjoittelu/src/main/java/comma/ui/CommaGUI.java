b6f5f62c6efa212d28590d226400878de158723f
Salla S <47078225+sallasal@users.noreply.github.com>
2020-11-21 10:08:15 +0200

    Separate LoggedInScene to its own class

--- Pilkkuharjoittelu/src/main/java/comma/ui/CommaGUI.java
+++ Pilkkuharjoittelu/src/main/java/comma/ui/CommaGUI.java
@@ -5,16 +5,9 @@
 package comma.ui;
 
 import javafx.application.Application;
-import javafx.event.*;
-import javafx.geometry.Insets;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.control.*;
-import javafx.scene.layout.*;
 import comma.domain.*;
-import java.sql.SQLException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 public class CommaGUI extends Application {
 
@@ -28,44 +21,15 @@ public class CommaGUI extends Application {
     @Override
     public void start(Stage window) {
 
-        //Main layout
         window.setTitle("Pilkkusääntöjen harjoittelu");
         window.setWidth(500.00);
         window.setHeight(500.00);
+        
+        //Create LoggedInScene
+        LoggedInScene loggedInScene = new LoggedInScene();
+        Scene loggedScene = loggedInScene.getLoggedInScene(commaService);
 
-        BorderPane basicLayout = new BorderPane();
-
-        HBox navigation = new HBox();
-        navigation.setPadding(new Insets(20, 20, 20, 20));
-        navigation.setSpacing(10);
-
-        Button practiceButton = new Button("Harjoittele");
-        Button createButton = new Button("Lisää tehtävä");
-        Button userButton = new Button("Käyttäjätiedot");
-        Label start = new Label("Aloita valitsemalla toiminto.");
-
-        //Note to self: add rest of the buttons to this very command when ready
-        navigation.getChildren().addAll(practiceButton, createButton, userButton);
-
-        basicLayout.setTop(navigation);
-
-        //Subscenes includind different functionalities
-        //For now, only practicing is working.
-        practiceButton.setOnAction((event) -> {
-            PracticeView practiceView = new PracticeView();
-            try {
-                basicLayout.setCenter(practiceView.getPracticeView(this.commaService.getRandomExercise()));
-            } catch (SQLException e) {
-                System.out.println(e.getMessage());;
-            }
-        });
-
-        basicLayout.setCenter(start);
-
-        //Creating and showing scene in GUI
-        Scene basicView = new Scene(basicLayout);
-
-        window.setScene(basicView);
+        window.setScene(loggedScene);
         window.show();
     }
 
---
+++ Pilkkuharjoittelu/src/main/java/comma/ui/LoggedInScene.java
@@ -0,0 +1,58 @@
+/**
+ *
+ * @author sallasal
+ */
+
+package comma.ui;
+
+import comma.domain.*;
+import comma.dao.*;
+import comma.ui.*;
+import java.sql.SQLException;
+import javafx.geometry.*;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+
+
+public class LoggedInScene {
+    
+    public LoggedInScene() {
+        
+    }
+    
+    public Scene getLoggedInScene(CommaService commaService) {
+        BorderPane basicLayout = new BorderPane();
+
+        HBox navigation = new HBox();
+        navigation.setPadding(new Insets(20, 20, 20, 20));
+        navigation.setSpacing(10);
+
+        Button practiceButton = new Button("Harjoittele");
+        Button createButton = new Button("Lisää tehtävä");
+        Button userButton = new Button("Käyttäjätiedot");
+        Label start = new Label("Aloita valitsemalla toiminto.");
+
+        //Note to self: add rest of the buttons to this very command when ready
+        navigation.getChildren().addAll(practiceButton, createButton, userButton);
+
+        basicLayout.setTop(navigation);
+
+        //Subscenes includind different functionalities
+        //For now, only practicing is working.
+        practiceButton.setOnAction((event) -> {
+            PracticeView practiceView = new PracticeView();
+            try {
+                basicLayout.setCenter(practiceView.getPracticeView(commaService.getRandomExercise()));
+            } catch (SQLException e) {
+                System.out.println(e.getMessage());;
+            }
+        });
+
+        basicLayout.setCenter(start);
+
+        
+        return new Scene(basicLayout);
+    }
+}
