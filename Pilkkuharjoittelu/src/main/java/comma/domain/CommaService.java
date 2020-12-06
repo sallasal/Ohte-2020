5c6272804f8f7d1c6c3d839c31a2e944e36e8cf5
Salla S <47078225+sallasal@users.noreply.github.com>
2020-12-06 14:05:24 +0200

    Add JavaDoc, comment CommaService

--- Pilkkuharjoittelu/src/main/java/comma/domain/CommaService.java
+++ Pilkkuharjoittelu/src/main/java/comma/domain/CommaService.java
@@ -1,3 +1,9 @@
+/**
+ * This class takes care of all interaction between database and GUI.
+ * Class fetches values from database and also writes to database via DAOs.
+ * Class also calculates parameter based values for db and GUI.
+ * Class also saves user information for each session.
+ */
 package comma.domain;
 
 import comma.domain.*;
@@ -20,11 +26,16 @@ public class CommaService {
         this.user = null;
     }
 
-    // User methods
+    /**
+     * Create a new user if username does not already exist
+     *
+     * @param username username that is going to be created
+     *
+     * @return boolean true, if user is created succesfully, false otherwise
+     */
     public boolean createUser(String username, String name) throws Exception {
         User userToAdd = new User(username, name);
 
-        //Validate here that this username does not already exist
         User validate = userDao.findByUsername(username);
 
         if (validate.getUsername() == null) {
@@ -35,6 +46,15 @@ public class CommaService {
         }
     }
 
+    /**
+     * Creates a new user if username does not already exist, defines an user
+     * for user session as class variable
+     *
+     * @param username username that is going to be validated
+     *
+     * @return boolean true, if username is free (does not already exist), false
+     * otherwise
+     */
     public boolean validateUsername(String username) throws Exception {
         User validate = userDao.findByUsername(username);
 
@@ -46,10 +66,26 @@ public class CommaService {
         }
     }
 
+    /**
+     * Fetches number of passed exercises in defined category for logged in user
+     *
+     * @param category category number from which the value is going to be
+     * fetched
+     *
+     * @return integer that shows how many exercises are completed from defined
+     * category
+     */
     public int getCompletedExercises(int category) throws Exception {
         return userDao.passedExercisesInCategory(this.user.getUsername(), category);
     }
 
+    /**
+     * Fetches number of passed exercises in defined category for logged in
+     * user, then adds +1 for it and replaces old value in database with new one
+     *
+     * @param category category number to which the completion is added
+     *
+     */
     public void addCompletion(int category) throws Exception {
         int currentCount = -2;
         if (category == 1) {
@@ -67,16 +103,30 @@ public class CommaService {
     public String getUsername() {
         return this.user.getUsername();
     }
-    
+
+    /**
+     * Nulls the user information in CommaService class parameter, used when
+     * user logs out
+     *
+     */
     public void nullUser() {
         this.user = null;
     }
 
+    /**
+     * Deletes an user from database based on username
+     *
+     * @param username username for user that is deleted
+     */
     public void deleteUser(String username) throws Exception {
         userDao.delete(username);
     }
 
-    // Exercise methods
+    /**
+     * Gets random exercise from all exercises in database for PracticeView
+     *
+     * @return Exercise object
+     */
     public Exercise getRandomExercise() throws SQLException {
         ArrayList<Exercise> exList = db.listAll();
 
@@ -85,6 +135,15 @@ public class CommaService {
         return exList.get(random.nextInt(exList.size()));
     }
 
+    /**
+     * Creates new exercise to the database
+     *
+     * @param firstpart String for sentence part before comma place
+     * @param secondpart String for sentence part after comma place
+     * @param comma Integer 1 if comma is needed, 0 otherwise (0 is default if any other value is given)
+     * @param category Integer for exercise category: 1 = main clause, 2 = subordinate clause, 3 = some other case
+     * @param user String for username that indicates who is adding exercise
+     */
     public void createExercise(String firstpart, String secondpart,
             int comma, int category, String user) throws Exception {
 
